89a5ae991d57aa7e4240f45a37d3301f747197b4
amongiove <amandamongiove@gmail.com>
2021-02-22 22:24:31 -0600

    [BOOK-7] create user home view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,10 @@
 class BooksController < ApplicationController
     
+    get '/books' do
+        erb :'books/books'
+    end
+
+    get '/books/new' do
+        erb :'books/new'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
                     flash[:message] = "That email is already in use."
                     redirect to("/signup") 
             else  
-                @user = User.create(username: params[:username], email: params[:email], password_digest: params[:password])
+                @user = User.create(username: params[:username].downcase, email: params[:email].downcase, password_digest: params[:password])
                 session['user_id'] = @user.id  
             end
             @user.save
@@ -41,15 +41,23 @@ class UsersController < ApplicationController
         @user = User.find_by(:username => params[:username], :password_digest => params[:password])
         if @user != nil
             session['user_id'] = @user.id
-            redirect to('/home')
         else
             flash[:message] = "Incorrect username and/or password."
             redirect to('/')
         end
+        redirect to("/home")
     end
 
     get '/home' do
-        puts "user_home page"
+        if logged_in?
+            @user = User.find_by_id(session['user_id'])
+            @books = @user.books
+            @user_books = @user.user_books
+            erb :'users/user_home'
+        else
+            flash[:message] = "You must be logged in to view this page."
+            redirect to("/")
+        end
     end
 
 end
