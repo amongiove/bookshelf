8579640bddd9d86c433a6a6529f94b817e957fd8
amongiove <amandamongiove@gmail.com>
2021-03-13 11:10:26 -0600

    Remove procfile txt extension

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'require_all'
 require 'dotenv'
     Dotenv.load('file1.env', 'file2.env')
 
-configure :production, :development do
+configure :development do
     ENV['SINATRA_ENV'] ||= "development"
 
     require 'bundler/setup'
@@ -14,6 +14,18 @@ configure :production, :development do
     ActiveRecord::Base.establish_connection(
         :adapter => "sqlite3",
         :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+    )
+end
+
+configure :production do
+    ENV['SINATRA_ENV'] ||= "production"
+
+    require 'bundler/setup'
+    Bundler.require(:default, ENV['SINATRA_ENV'])
+
+    ActiveRecord::Base.establish_connection(
+        :adapter => "postgresql",
+        :database => "db/#{ENV['SINATRA_ENV']}.postgresql"
     )
 end
 
